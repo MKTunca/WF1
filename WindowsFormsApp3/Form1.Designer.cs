682aeedeeca05c729e94f3d6a57c5d616c250015
MKTunca <91753517+MKTunca@users.noreply.github.com>
2021-12-27 00:18:19 +0300

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sayi1 = new System.Windows.Forms.TextBox();
+            this.sayi2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sonuc = new System.Windows.Forms.Button();
+            this.epostaTXT = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(150, 26);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(100, 96);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sayi1
+            // 
+            this.sayi1.Location = new System.Drawing.Point(133, 180);
+            this.sayi1.Name = "sayi1";
+            this.sayi1.Size = new System.Drawing.Size(100, 20);
+            this.sayi1.TabIndex = 2;
+            // 
+            // sayi2
+            // 
+            this.sayi2.Location = new System.Drawing.Point(257, 180);
+            this.sayi2.Name = "sayi2";
+            this.sayi2.Size = new System.Drawing.Size(100, 20);
+            this.sayi2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x";
+            // 
+            // sonuc
+            // 
+            this.sonuc.Location = new System.Drawing.Point(375, 178);
+            this.sonuc.Name = "sonuc";
+            this.sonuc.Size = new System.Drawing.Size(75, 23);
+            this.sonuc.TabIndex = 5;
+            this.sonuc.Text = "sonuc";
+            this.sonuc.UseVisualStyleBackColor = true;
+            this.sonuc.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // epostaTXT
+            // 
+            this.epostaTXT.Location = new System.Drawing.Point(133, 271);
+            this.epostaTXT.Name = "epostaTXT";
+            this.epostaTXT.Size = new System.Drawing.Size(100, 20);
+            this.epostaTXT.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "giriş";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.epostaTXT);
+            this.Controls.Add(this.sonuc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sayi2);
+            this.Controls.Add(this.sayi1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox sayi1;
+        private System.Windows.Forms.TextBox sayi2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sonuc;
+        private System.Windows.Forms.TextBox epostaTXT;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int x = 5; x >= 1; x--)
+            {
+                for (int y = x; y >= 1; y--)
+                {
+                    rtb.Text = rtb.Text + "*";
+                }
+            }
+            rtb.Text = rtb.Text + "\r\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sayi1.Text = sayi1.Text.Replace(".", ",");
+
+            sayi2.Text = sayi2.Text.Replace(".", ",");
+            double s1 = Convert.ToDouble(sayi1.Text);
+            double s2 = Convert.ToDouble(sayi2.Text);
+            double deger = s1 * s2;
+            sonuc.Text = deger.ToString();
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Int32 indexNo = epostaTXT.Text.IndexOf("@");
+            if(indexNo!=-1)
+            {
+                if(indexNo!=0)
+                {
+                    MessageBox.Show("doğrulandı");
+                }
+            }   else
+            {
+                MessageBox.Show("hatalı");
+            }
+                
+        }
+    }
+}
